7b4356a3f5339c71f18428f70b970fa4aa009874
Ayoola Adags <ayoola.adagunodo@redemptive.nyc>
2024-11-12 15:30:54 -0800

    update verufy

--- app/api/metahook/route.js
+++ app/api/metahook/route.js
@@ -19,3 +19,17 @@ export async function POST(req) {
     return NextResponse.json({ error: 'Failed to receive' }, { status: 500 });
   }
 }
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const mode = searchParams.get("hub.mode");
+  const token = searchParams.get("hub.verify_token");
+  const challenge = searchParams.get("hub.challenge");
+
+  if (mode === "subscribe" && token === "vorsto2024-11-12") {
+    console.log("Webhook verified successfully!");
+    return NextResponse.json(challenge, { status: 200 });
+  } else {
+    return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+  }
+}
