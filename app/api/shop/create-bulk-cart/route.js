0eff5d3c6422251836e9c83e47cb71030dde254b
Ayoola Adags <ayoola.adagunodo@redemptive.nyc>
2025-05-18 11:36:41 -0700

    create bulk

---
+++ app/api/shop/create-bulk-cart/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import prisma from "@/db/prisma";
+import { cookies } from "next/headers";
+
+export const dynamic = "force-dynamic";
+
+
+export async function POST(req) {
+  try {
+
+    const body = await req.json();
+    const { uuid, org_id, cart } = body;
+
+    const org = await prisma.organization.findFirstOrThrow({
+      where: { id: org_id },
+    });
+    
+    let existingCart = await prisma.cart.findFirst({
+      where: {
+        uuid, organization_id: org.id
+      }
+    })
+
+    if(!existingCart)
+    {
+      existingCart = await prisma.cart.create({
+        data: {
+          uuid,
+          organization_id: org.id,
+        }
+      });
+    }
+
+    await prisma.cartItem.createMany({
+      data: cart.map(item => ({
+        inventory_id: item.inventory_id,
+        cart_id: existingCart.id,
+        quantity: item.quantity,
+      })),
+    });
+    
+
+
+    return NextResponse.json({ status: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch org" },
+      { status: 500 }
+    );
+  }
+}
