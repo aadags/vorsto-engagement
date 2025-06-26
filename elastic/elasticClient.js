57b7c482380c8ddb5f4085dda0ac50d6e0a1d610
Ayoola Adags <ayoola.adagunodo@redemptive.nyc>
2025-06-26 00:08:34 -0700

    tty

--- elastic/elasticClient.js
+++ elastic/elasticClient.js
@@ -4,8 +4,8 @@ import { Client } from '@elastic/elasticsearch';
 export const esClient = new Client({
   node: `${process.env.ELASTIC_SEARCH_URL}`, // or your hosted endpoint
   auth: {
-    username: 'elastic',
-    password: 'vorsto2025%%'
+    username: `${process.env.ELASTIC_SEARCH_USER}`,
+    password: `${process.env.ELASTIC_SEARCH_PASS}`
   },
   tls: {
     rejectUnauthorized: false
