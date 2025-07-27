3e187f5d1408f6732cc9a5b01a35ee2b6263b7b1
Ayoola Adags <ayoola.adagunodo@redemptive.nyc>
2025-07-27 14:48:25 -0700

    fix data

--- app/api/pos/get-products/route.js
+++ app/api/pos/get-products/route.js
@@ -9,16 +9,27 @@ export async function POST(req) {
   try {
 
     const body = await req.json();
-    const { id , categoryId} = body;
+    const { id, categoryId } = body;
 
-
+    // build base filter
+    const where = {
+      organization_id: id,
+      active: true,
+    };
+    
+    // if we're not in “Recent” mode, add the category filter
+    if (categoryId !== 'Recent') {
+      where.category_id = categoryId;
+    }
+    
     const products = await prisma.product.findMany({
-      where: { organization_id: id, category_id: categoryId, active: true },
-      include:{
+      where,
+      include: {
         inventories: {
           where: { active: true },
         },
-      }
+      },
+      take: 16
     });
 
     return NextResponse.json({ products });
