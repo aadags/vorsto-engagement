b8444c8314ccc8ea7007f3e692495aafaf1c6b54
Ayoola Adags <ayoola.adagunodo@redemptive.nyc>
2025-09-24 13:07:08 -0700

    add dev

---
+++ app/api/order/get-delivery-order/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import prisma from "@/db/prisma";
+import { cookies } from "next/headers";
+
+export const dynamic = "force-dynamic";
+
+
+export async function GET(req) {
+  try {
+
+    const id = req.nextUrl.searchParams.get("id");
+
+    const order = await prisma.order.findFirst({
+      where: { shipping_id: id 
+      },
+      include: {
+        order_items: {
+          include: {
+            inventory: {
+              include: {
+                product: true
+              }
+            }
+          }
+        },
+        contact: true
+      }
+    });
+
+    return NextResponse.json({ ...order });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch order" },
+      { status: 500 }
+    );
+  }
+}
